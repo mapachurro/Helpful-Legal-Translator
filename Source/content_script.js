aba429e8be9a2537cce1cbdd2422ae829306094a
oliver <oliver.renwick@gmail.com>
2019-12-15 17:08:31 -0500

    reworking manifest.json, first terms in content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -39,8 +39,8 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
+	v = v.replace(/\bmaybe\b/g, "allegedly");
+	v = v.replace(/\bperhaps\b/g, "allegedly");
 	v = v.replace(/\bthe Cloud\b/g, "my Butt");
 	v = v.replace(/\bthe cloud\b/g, "my butt");
 	
